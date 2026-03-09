f03967e4d697a4fa6fdd3acecf7c2943a9f716dd
n <n>
2026-03-09 04:20:38 +0000

    agents deploy

--- apps/insighthunter-agents/deployagents.sh
+++ apps/insighthunter-agents/deployagents.sh
@@ -5,6 +5,8 @@ git push
 export CLOUDFLARE_ACCOUNT_ID="18c8e61a3669253dcfd0c7eec6be36a3"
 export CLOUDFLARE_API_TOKEN="kAY9u88TaeuI9wByQkismZ2oGjBWqf5mVBhDTYNE"
 
+# pnpm install apps/insighthunter-agents 
+
 pnpm --filter @insighthunter/agents dev
 
 pnpm --filter @insighthunter/agents deploy
