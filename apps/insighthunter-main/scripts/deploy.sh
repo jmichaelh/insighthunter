78ac57010eba0aa88ca4aa2dfd2f425d772529ed
n <n>
2026-03-11 16:17:01 +0000

    :pre-deploy git

--- apps/insighthunter-main/scripts/deploy.sh
+++ apps/insighthunter-main/scripts/deploy.sh
@@ -44,7 +44,7 @@ set -e
 
 # Deploy insighthunter-main
 echo "Deploying insighthunter-main..."
-npx wrangler pages deploy --config ./wrangler.toml
+npx wrangler pages deploy 
 
 # Deploy insighthunter-bookkeeping
 # echo "Deploying insighthunter-bookkeeping..."
